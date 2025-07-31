44ac9f03db222eb1ee808c2b5cfb43095c3ab120
Havrylenko Illia <76633174+HID-KPI-Karazin@users.noreply.github.com>
2025-07-31 07:00:05 +0300

    Revert "Implement Notion pagination connector"

--- tests/__mocks__/p-queue.ts
+++
@@ -1,8 +0,0 @@
-export default class PQueue {
-  // Minimal mock just executes tasks immediately
-  // eslint-disable-next-line no-unused-vars
-  constructor(public opts: any) {}
-  async add<T>(fn: () => Promise<T>): Promise<T> {
-    return fn();
-  }
-}
